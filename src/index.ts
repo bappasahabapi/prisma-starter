bd3632697b508fac4c4e4b7f617f1fddadeb9a6a
Bappa Saha <bappasaha161@gmail.com>
2023-08-30 23:35:56 +0600

    [05] make the MVC file structure

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import express, { Application } from "express";
-import cors from 'cors';
+import app from "./app";
 
-
-
-
-const app:Application = express();
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
 
 
 const prisma =new PrismaClient();
@@ -18,7 +10,6 @@ async function main() {
     app.listen(port,()=>{
         console.log(`server is listening on ${port}`)
     })
- 
     
 }
 main();
